ec2406187bfa5f4be9b6001c84e960071f33db52
Adrian <adrianwor@wp.pl>
2023-04-12 14:46:10 +0200

    Dodano opcję dodawania filmu poprzez formularz (bez weryfikacji nazwy przesyłanego pliku)

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -1,7 +1,10 @@
 ﻿using AdrianWoronaProject91511.DAL;
 using AdrianWoronaProject91511.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
 using System.Linq;
 
 namespace AdrianWoronaProject91511.Controllers
@@ -9,10 +12,12 @@ namespace AdrianWoronaProject91511.Controllers
     public class FilmsController : Controller
     {
         FilmsContext db;
+        IWebHostEnvironment env;
 
-        public FilmsController(FilmsContext db)
+        public FilmsController(FilmsContext db, IWebHostEnvironment env)
         {
             this.db = db;
+            this.env = env;
         }
 
         public IActionResult FilmsList(string categoryName)
@@ -20,7 +25,6 @@ namespace AdrianWoronaProject91511.Controllers
             var model = new CategoryFilmsViewModel();
 
             var category = db.Categories.Include("Films").Where(c => c.Name.ToUpper() == categoryName.ToUpper()).Single();
-
             var films = category.Films.ToList();
 
             model.CategoryFilms = films;
@@ -37,6 +41,34 @@ namespace AdrianWoronaProject91511.Controllers
 
             var film = db.Films.Include("Category").Where(f => f.Id == filmId).Single();
             return View(film);
+        }
+
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            var model = new AddFilmViewModel();
+            var categories = db.Categories.ToList();
+
+            model.Categories = categories;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(AddFilmViewModel model)
+        {
+            var folderPath = Path.Combine(env.WebRootPath, "content");
+            var posterPath = Path.Combine(folderPath, model.Poster.FileName);
+
+            model.Poster.CopyTo(new FileStream(posterPath, FileMode.Create));
+
+            model.NewFilm.PublishDate = DateTime.Now;
+
+            model.NewFilm.PosterName = model.Poster.FileName;
+            db.Films.Add(model.NewFilm);
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ ViewModels/AddFilmViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AdrianWoronaProject91511.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace AdrianWoronaProject91511.ViewModels
+{
+    public class AddFilmViewModel
+    {
+        public Film NewFilm { get; set; }
+
+        public List<Category> Categories { get; set; }
+
+        public IFormFile Poster { get; set; }
+    }
+}
